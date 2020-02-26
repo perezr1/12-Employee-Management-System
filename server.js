dba851fdcfcfbd86c71810138e761376884017e2
“Ronny <contact@ronnyperez.com>
2020-02-25 21:04:22 -0600

    commiting for the night

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Initializes the npm packages used
-var mysql = require("mysql2");
+var mysql2 = require("mysql2");
 var inquirer = require("inquirer");
 require("console.table");
 
@@ -8,7 +8,7 @@ var connection = mysql2.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Jacoban27-",
   database: "employee_trackerDB"
 });
 
@@ -23,15 +23,16 @@ function makeTable() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.table(res);
-    promptSupervisor();
+    runSearch();
   });
 }
 
 function runSearch() {
   inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
+    .prompt(
+      {
+      name: "Choice",
+      type: "list",
       message: "What would you like to do?",
       choices: [
         "Add department",
@@ -64,5 +65,37 @@ function runSearch() {
           viewEmployee();
           break;
       }
+    });
+}
+
+function addDepartment() {
+  // Asking the user about the department they would like to add
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "What is the ID of the department?"
+      },
+      {
+        type: "input",
+        name: "departementId",
+        message: "What is the name of the department",
+        validate: function(val) {
+          return val > 0;
+        }
+      }
+    ])
+    .then(function(answer) {
+      // Using the information the user provided to create a new department
+      connection.query("INSERT INTO department (id,name) VALUES (?, ?)",
+        [val.id, val.name],
+        function(err) {
+          if (err) throw err;
+          // If successful, alert the user, run makeTable again
+          console.log("ADDED DEPARTMENT!");
+          makeTable();
+        }
+      );
     });
 }
